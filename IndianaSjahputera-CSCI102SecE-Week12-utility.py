af5f6617b9c1e75f380e136542da5ed96be0c25e
isjahputera <57501190+isjahputera@users.noreply.github.com>
2019-11-21 11:05:40 -0700

    Function 2 - must check

--- IndianaSjahputera-CSCI102SecE-Week12-utility.py
+++ IndianaSjahputera-CSCI102SecE-Week12-utility.py
@@ -13,8 +13,48 @@ def PrintOutput(string):
 
 def LoadFile(file):
     o_file = open(file, "r")
-    r_file = file.readlines()
+    with o_file as f:
+        f_list = list(f)
     return r_file
 
+# Function 3 - UpdateString
+
+def UpdateSetting(string1, string2, num):
+    new_string = string1[:num] + string2 + string1[num + 1:]
+    print("OUTPUT", new_string)
+
+# Function 4 - FindWordCount
+
+def FindWordCount(f_file, fstring1):
+    counts = f_file.count(fstring1)
+    print("OUTPUT", counts)
     
+
+# Function 5 - ScoreFinder
+
+def ScoreFinder(players, scores, name):
+    if name in players:
+        for i in enumerate(name, start=0):
+            n_score = scores.index[i]
+        print("OUTPUT", name, "got a score of ", n_score)
+    else:
+        print("OUTPUT player not found")
+
+# Function 6 - Union
+
+def Union(list1, list2):
+    list3 = list1 + list2
+    print("OUTPUT", list3)
+
+# Function 7 - Intersection
+
+def Intersection(ilist1, ilist2):
+    n_ilist = list(set(ilist1) & set(ilist2))
+    print("OUTPUT", n_ilist)
+
+# Function 8 - NotIn
+
+def NotIn(nlist1, nlist2):
+    n_nlist = set(nlist1).difference(nlist2)
+    print("OUTPUT", n_nlist)
     
