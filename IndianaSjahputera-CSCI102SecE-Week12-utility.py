baeff288ed9dca18ae9993efab94a7fc16975ad7
isjahputera <57501190+isjahputera@users.noreply.github.com>
2019-11-21 14:16:41 -0700

    All functions work - God Bless America

--- IndianaSjahputera-CSCI102SecE-Week12-utility.py
+++ IndianaSjahputera-CSCI102SecE-Week12-utility.py
@@ -4,62 +4,71 @@
 # Week 11 - Part B
 # The GITHUB REPO
 
-# Function 1 - PrintOutput from previous lab
+# Function 1 - PrintOutput from previous lab 
 
 def PrintOutput(string):
     print('OUTPUT', string)
 
 
-# Function 2 - LoadFile
+# Function 2 - LoadFile 
 
 def LoadFile(file):
     o_file = open(file, "r")
+    e_list = []
     with o_file as f:
-        f_list = list(f)
-    return r_file
+        f_list = f.readlines()
+        for line in f_list:
+            e_list.append(line.strip())
+    return e_list
 
 
-# Function 3 - UpdateString
+# Function 3 - UpdateString 
 
 def UpdateSetting(string1, string2, num):
     new_string = string1[:num] + string2 + string1[num + 1:] #Saw this online/spoke with people
     print("OUTPUT", new_string)
 
 
-# Function 4 - FindWordCount
+# Function 4 - FindWordCount 
 
 def FindWordCount(f_file, fstring1):
-    counts = f_file.count(fstring1)
-    print("OUTPUT", counts)
+    n_list = []
+    counts = 0
+    for i in f_file:
+        n_list = i.split()
+        counts += n_list.count(fstring1)
+    return counts
     
 
-# Function 5 - ScoreFinder
+# Function 5 - ScoreFinder 
 
 def ScoreFinder(players, scores, name):
-    if name in players:
-        for i in enumerate(name, start=0):
-            n_score = scores.index[i]
-        print("OUTPUT", name, "got a score of ", n_score)
+    c_name = name.capitalize()
+    if c_name in players:
+        n_score = players.index(c_name)
+        p1 = print("OUTPUT", name, "got a score of ", scores[n_score])
+        return p1
     else:
-        print("OUTPUT player not found")
+        p2 = print("OUTPUT player not found")
+        return p2
 
 
-# Function 6 - Union
+# Function 6 - Union 
 
 def Union(list1, list2):
     list3 = list1 + list2
-    print("OUTPUT", list3)
+    return list3
 
 
-# Function 7 - Intersection
+# Function 7 - Intersection 
 
 def Intersection(ilist1, ilist2):
     n_ilist = list(set(ilist1) & set(ilist2))
-    print("OUTPUT", n_ilist)
+    return n_ilist
 
 # Function 8 - NotIn
 
 def NotIn(nlist1, nlist2):
     n_nlist = set(nlist1).difference(nlist2)
-    print("OUTPUT", n_nlist)
+    return n_nlist
     
