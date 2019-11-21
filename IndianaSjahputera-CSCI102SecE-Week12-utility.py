20bb9537a02ec75194cf81eeae116914f2644151
isjahputera <57501190+isjahputera@users.noreply.github.com>
2019-11-21 10:38:52 -0700

    Processing def functions - must run to check

--- IndianaSjahputera-CSCI102SecE-Week12-utility.py
+++ IndianaSjahputera-CSCI102SecE-Week12-utility.py
@@ -1,3 +1,4 @@
+# https://github.com/isjahputera/CSCI102_wk12_A.git
 # Indiana Sjahputera
 # CSCI 102 - Section E
 # Week 11 - Part B
@@ -7,3 +8,13 @@
 
 def PrintOutput(string):
     print('OUTPUT', string)
+
+# Function 2 - LoadFile
+
+def LoadFile(file):
+    o_file = open(file, "r")
+    r_file = file.readlines()
+    return r_file
+
+    
+    
